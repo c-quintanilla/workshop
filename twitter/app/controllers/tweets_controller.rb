e2e1aebaf7b086328c77d285dadb976c9063a41b
webtech <webtech@webtech-debian.webtech>
2023-06-10 16:29:43 -0400

    Crud ready

--- twitter/app/controllers/tweets_controller.rb
+++ twitter/app/controllers/tweets_controller.rb
@@ -23,7 +23,7 @@ class TweetsController < ApplicationController
   # POST /tweets or /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
-
+    @tweet.user = current_user
     respond_to do |format|
       if @tweet.save
         format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
@@ -66,6 +66,6 @@ class TweetsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def tweet_params
-      params.require(:tweet).permit(:body, :user_id)
+      params.require(:tweet).permit(:body)
     end
 end
